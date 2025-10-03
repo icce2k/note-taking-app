073d7d061085d223c3cf73a1837a7f00569555b1
Isaiah Eusebio <isaiahc.eusebio@gmail.com>
2025-10-03 14:57:38 -0700

    set up nodemon and basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.send("you got 5 notes");
+  res.status(200).send("you got 20 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({message: "post created successfully!"});
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({message: "post updated successfully!"});
+});
+
+app.delete("api/notes/:id", (req, res) => {
+  res.status(200).json({message: "post deleted successfully!"});
 });
 
 app.listen(5001, () => {
