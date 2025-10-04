02946a0b715668bbe529641a65d91b3d5b793f91
Isaiah Eusebio <isaiahc.eusebio@gmail.com>
2025-10-04 00:53:12 -0700

    optimized file structure
    created separate routes and controller files

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,9 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-  res.status(200).send("you got 20 notes");
-});
-
-app.post("/api/notes", (req, res) => {
-  res.status(201).json({message: "post created successfully!"});
-});
-
-app.put("/api/notes/:id", (req, res) => {
-  res.status(200).json({message: "post updated successfully!"});
-});
-
-app.delete("api/notes/:id", (req, res) => {
-  res.status(200).json({message: "post deleted successfully!"});
-});
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
   console.log("Server is running on PORT: 5001");
