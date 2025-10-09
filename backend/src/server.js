28c12c9021cc16b3dd3a96b4a7cd6ac0dbdcd2b5
Isaiah Eusebio <isaiahc.eusebio@gmail.com>
2025-10-08 23:35:01 -0700

    finished backend and frontend
    routing, rate limiting, ui, and functionality. connected frontend and backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,29 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+app.use(rateLimiter);
+
+// app.use((req, res, next) => {
+//   console.log("We just got a new request!");
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on PORT: 5001");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on PORT:", PORT);
+  });
 });
+
